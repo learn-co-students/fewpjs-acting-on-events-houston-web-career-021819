b3971784fd32ff59b6726e45a9b6fc2d4f126b83
github username <github email address>
2019-04-03 13:27:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+let dodger = document.querySelector("#dodger")
+
+function moveDodgerLeft() {
+  dodger.style.left = "0px";   
+}
+function moveDodgerRight() {
+    dodger.style.left = "360px";   
+  }
